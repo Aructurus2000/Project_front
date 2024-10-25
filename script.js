1242ff060662e490d0ccb68febb79d2e173a2010
arthur_burzhumov <arturb2000@mail.ru>
2024-10-25 20:34:39 +0300

    Синтаксис переменных, строгий режим

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Привет");
+// Синтаксис переменных
+//"use strict"; // строгий режим
+// console.log(message1);
+// var message1 = "Привет"; // можно использоваь до фактического применения
+
+// let message = "Привет";
+// console.log(message);
+// message = "Пока";
+// console.log(message);
+
+// const name = "Александр",
+//   age = 21,
+//   isDeveloper = true;
+// const admin = name;
+// console.log(admin);
+
+// admin = 1;
+// console.log(admin);
