ce1fcfb81f88460cbe7e4c85cc9d1c1a9ab2a68c
arthur_burzhumov <arturb2000@mail.ru>
2024-10-25 23:26:33 +0300

    Проверка типов (typeof), преобразование типов, интерполяция строк

--- script.js
+++ script.js
@@ -1,18 +1,41 @@
-// Синтаксис переменных
-//"use strict"; // строгий режим
-// console.log(message1);
-// var message1 = "Привет"; // можно использоваь до фактического применения
+//Проверка типов (typeof), преобразование типов, интерполяция строк
+// const name = "Александр";
+// const goobye = `Пока`;
 
-// let message = "Привет";
-// console.log(message);
-// message = "Пока";
+//интерполяция строк
+// const message = `${goobye}, ${name}!`;
 // console.log(message);
 
-// const name = "Александр",
-//   age = 21,
-//   isDeveloper = true;
-// const admin = name;
-// console.log(admin);
+// const a = 10;
+// const b = 5;
+// const sum = `Сумма a и b  равна ${a + b}`;
+// console.log(sum);
 
-// admin = 1;
-// console.log(admin);
+// //bigint
+// console.log(9900000000000091n + 1n);
+// console.log(9900000000000091n + 2n);
+
+//typeof
+// const message = "Привет";
+// console.log(typeof message);
+
+//Неявные преобразования
+// const num = 111;
+// const str = "222";
+// const result = num + str; //происходит преобразование из number в string
+// console.log(result);
+// console.log(typeof result);
+// console.log("16" / "8"); //происходит преобразование из string в number
+
+//Явное преобразование типов
+// const age = 11;
+// console.log(typeof age);
+// console.log(typeof String(age)); // глобавльная функция String
+// const age1 = "22";
+// console.log(typeof age1);
+// console.log(typeof Number(age1)); // глобавльная функция Number
+
+// console.log(Boolean(-1)); // глобавльная функция Boolean
+// console.log(Boolean(0));
+// console.log(Boolean(1));
+// console.log(Boolean(2));
